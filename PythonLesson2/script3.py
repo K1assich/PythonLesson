288470081884b6dd09194d0d8309350d19839baa
K1assich <87496292+K1assich@users.noreply.github.com>
2022-12-05 23:47:40 +0300

    Add files via upload
    Lesson2

---
+++ PythonLesson2/script3.py
@@ -0,0 +1,3 @@
+str = input('Input string: ')
+print(f'The last two characters in reverse order: {str[len(str)-1]}{str[len(str)-2]}')
+#Нет проверки на длину строки, чтобы избежать выхода из массива, но не знаю как и его вместить в две строки
---
+++ PythonLesson2/script4.py
@@ -0,0 +1,4 @@
+import math
+radius = input('Input radius: ')
+radius = float(radius)
+print('Circle radius: ', math.pi*radius*radius)
